8d766d9686157096bdd967599bf9c1db75dbba25
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2024-12-14 19:39:04 -0600

    Time: 0 ms (100%), Space: 17.6 MB (9.38%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res, cur = [], []
+
+        def dfs():
+            if len(cur) == n:
+                res.append(cur.copy())
+                return
+
+            for i in nums:
+                if i not in cur:
+                    cur.append(i)
+                    dfs()
+                    cur.pop()
+
+        dfs()
+        return res
+                    
+            
+
+        
