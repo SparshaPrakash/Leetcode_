b95780a45dc4d194669c9e0f221c927437d4b07b
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2024-12-23 22:17:49 -0600

    Time: 0 ms (100%), Space: 18.4 MB (6.14%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,30 +1,56 @@
-# class Solution:
-#     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        par = [i for i in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def find(n):
+            p = par[n]
+            while p != par[p]:
+                par[p] = par[par[p]]
+                p = par[p]
+            return p
+
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
         
 
 # DFS solution
-class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        adj = [[] for _ in range(n + 1)]
+# class Solution:
+#     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+#         n = len(edges)
+#         adj = [[] for _ in range(n + 1)]
 
-        def dfs(node, par):
-            if visit[node]:
-                return True
+#         def dfs(node, par):
+#             if visit[node]:
+#                 return True
             
-            visit[node] = True
-            for nei in adj[node]:
-                if nei == par:
-                    continue
-                if dfs(nei, node):
-                    return True
-            return False
+#             visit[node] = True
+#             for nei in adj[node]:
+#                 if nei == par:
+#                     continue
+#                 if dfs(nei, node):
+#                     return True
+#             return False
         
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-            visit = [False] * (n + 1)
+#         for u, v in edges:
+#             adj[u].append(v)
+#             adj[v].append(u)
+#             visit = [False] * (n + 1)
             
-            if dfs(u, -1):
-                return [u, v]
-        return []
+#             if dfs(u, -1):
+#                 return [u, v]
+#         return []
