c2d9cc6255aedcc69d24c749916ea1c5f9c76c9a
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-01-22 22:22:40 -0600

    Time: 153 ms (42.08%), Space: 23.1 MB (39.55%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,20 +1,20 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        dp = {} # to store l, r pairs
+        n = len(piles)
+        dp = [[0] * n for _ in range(n)]
 
-        def dfs(l, r):
-            if l > r:
-                return 0
-            if (l, r) in dp:
-                return dp[(l, r)]
+        # Base case: If there's only one pile, the player takes it.
+        for i in range(n):
+            dp[i][i] = piles[i]
 
-            even = True if (r - 1) % 2 else False
-            left = piles[l] if even else 0
-            right = piles[r] if even else 0
+        # Fill DP table for ranges of increasing length
+        for length in range(2, n + 1):
+            for l in range(n - length + 1):
+                r = l + length - 1
+                dp[l][r] = max(
+                    piles[l] - dp[l + 1][r],  # Choose left pile
+                    piles[r] - dp[l][r - 1]   # Choose right pile
+                )
 
-            dp[(l, r)] = max(dfs(l + 1, r) + left, dfs(l, r - 1) + right)
-        
-            return dp[(l, r)]
-
-        return dfs(0, len(piles) - 1) > sum(piles) // 2
-        
+        # If Alex (first player) can score more than Lee (second player), return True
+        return dp[0][n - 1] > 0
