77bfb1e578c9d5ff4622a412e198989aaa2674f0
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-02-19 13:54:12 -0600

    Time: 355 ms (47.95%), Space: 17.7 MB (84.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,61 +1,25 @@
 class Solution:
     
-    
     def longestPalindrome(self, s: str) -> str:
-        self.res = ""
-        self.res_len = 0
+        self.res =""
+        self.res_length = 0 
+
+        # odd palindrome
 
         for i in range(len(s)):
-            # Odd palindrome
             l, r = i, i
-            self.expand_check(l, r, s)
+            self.expand(l,r,s)
 
-            # Even palindrome
+            # even pailndrome
             l, r = i, i + 1
-            self.expand_check(l, r, s)
+            self.expand(l,r,s)
 
         return self.res
 
-    def expand_check(self, l, r, s):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            if (r - l + 1) > self.res_len:
-                self.res = s[l: r + 1]
-                self.res_len = r - l + 1
-            l -= 1
+    def expand(self, l, r, s):
+        while (l >= 0 and r <= len(s) - 1 and s[l] == s[r]):
+            if (r - l + 1) > self.res_length:
+                self.res = s[l:r + 1]
+                self.res_length = r - l + 1
             r += 1
-
-
-
-# class Solution:
-
-#     def __init__(self):
-#         self.res = ""
-#         self.res_len = 0
-
-#     def expand_check(self, l, r, s):
-#         while l >= 0 and r < len(s) and s[l] == s[r]: 
-#             if (r - l + 1) > self.res_len:
-#                 self.res = s[l: r + 1]
-#                 self.res_len = r - l + 1
-#             l -= 1
-#             r += 1
-    
-#     def longestPalindrome(self, s: str) -> str:
-#         # brute force -
-#         # O(n^2) check every combo and O(n) see if it's a palindrome => O (n^3)
-#         # 2 pointer approach start from center
-            
-
-#         for i in range(len(s)):
-#             # consider this as center and traverse checking for odd pali and even pali
-            
-#             # odd pali check
-#             l, r = i, i
-#             # i = 2, l = 1, r = 3, len = 3 so r - l + 1
-#             self.expand_check(l, r, s)
-            
-#             # for even
-#             l, r = i, i + 1
-#             self.expand_check(l, r, s)
-
-#         return self.res
+            l -= 1
