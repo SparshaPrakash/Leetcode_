68d04754ee5dacec6c2efcda314cafc21bf57a05
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-02-20 19:15:18 -0600

    Time: 0 ms (100%), Space: 17.9 MB (49.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
         M, N = len(grid), len(grid[0])
-        dp = {(M - 1, N - 1) : 1}
+        dp = [0] * N
+        dp[N - 1] = 1
 
-        def dfs(r, c):
-            if r == M or c == N or grid[r][c]:
-                return 0
+        # here Time: O(M*N); Space: O(N)
+        for r in reversed(range(M)):
+            for c in reversed(range(N)):
+                if grid[r][c] == 1: # its an obstacle
+                    dp[c] = 0 # its only c cause here we are doing the calculations in place
+                elif c + 1 < N:# it was c + 1 = N, then we went out of bounds
+                    dp[c] = dp[c] + dp[c + 1]   # here dp[c] is the bottom value and dp[c+1] is the right value
 
-            if (r, c) in dp:
-                return dp[(r,c)]
-
-            dp[(r,c)] = dfs(r + 1, c) + dfs(r, c + 1)
-            return dp[(r, c)]
-        return dfs(0, 0)
+                else:
+                    dp[c] = dp[c] + 0 # we dont really need this else case
+        return dp[0]
