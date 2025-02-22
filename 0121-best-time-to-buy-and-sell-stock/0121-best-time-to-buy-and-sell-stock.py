50b43dcb887a606eb997e029d9857902e9ae1cf8
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-02-22 15:11:19 -0600

    Time: 143 ms (12.34%), Space: 26.8 MB (77.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices:
-            return 0 
+            return 0
+        
+        min_buy = prices[0]  # Start with the first price as the initial minimum price
+        mx_profit = 0  # Initialize the maximum profit as 0
+        
+        for i in range(1, len(prices)):  # Start from the second price onward
+            # Calculate profit if sold at current price
+            profit = prices[i] - min_buy
+            
+            # Update maximum profit
+            mx_profit = max(mx_profit, profit)
+            
+            # Update minimum buy price if a lower price is found
+            min_buy = min(min_buy, prices[i])
+        
+        return mx_profit
 
-        min_price = prices[0]
-        max_profit = 0
-
-        for price in prices:
-            profit = price - min_price
-
-            max_profit = max(max_profit, profit)
-
-            min_price = min(min_price, price)
-
-        return max_profit
+        
