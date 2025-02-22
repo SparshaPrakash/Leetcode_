d0f0c1d9312d71ecbd9b560a6da6068bb0d46294
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-02-21 20:06:02 -0600

    Time: 18 ms (75.73%), Space: 18.9 MB (45.81%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -6,26 +6,13 @@ class Solution:
         :param matrix: List[List[int]] - a square matrix of integers
         :return: int - the minimum falling path sum
         """
-        # recursive solution
         N = len(matrix)
-        cache = {}
+
+        for r in range(1, N):
+            for c in range(N):
+                mid = matrix[r - 1][c]
+                left = matrix[r - 1][c - 1] if c > 0 else float("inf")
+                right = matrix[r - 1][c + 1] if c < N - 1 else float("inf")
+                matrix[r][c] = matrix[r][c] + min(mid, left, right)
+        return min(matrix[-1])
         
-        def dfs(r, c):
-            if r == N:
-                return 0
-            if c < 0 or c == N:
-                return float("inf")   # retrunign a huge number so its going to not add any value in the minimum sum path
-            if (r, c) in cache:
-                return cache[(r, c)]
-
-            res = matrix[r][c] + min(dfs(r + 1, c), dfs(r + 1, c - 1), dfs(r + 1, c + 1))
-
-            cache[(r, c)] = res
-
-            return res
-
-
-        res = float("inf")
-        for c in range(N):
-            res = min(res, dfs(0, c))
-        return res
