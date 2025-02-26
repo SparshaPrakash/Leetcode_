60dce4f74ca24ab577b8c22efd569e7dfd3ce09a
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-02-26 11:11:36 -0600

    Time: 0 ms (100%), Space: 18.6 MB (92.98%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        dp = [0] * (len(triangle) + 1)
+        dp = [0] * (len(triangle) + 1) # because the next row is always has one column extra, we ponly keep track of the row below
 
         for row in triangle[::-1]:
             for i, n in enumerate(row):
