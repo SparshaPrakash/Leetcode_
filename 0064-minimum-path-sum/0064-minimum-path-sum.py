a1c1fcafa7e68ee3ecc123cf2b3329c5c659dfe8
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-03-08 13:36:25 -0600

    Time: 11 ms (90.96%), Space: 20.5 MB (29.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,15 +1,12 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [0 for _ in range(len(grid[0]))]
-        for i in range(len(grid) - 1, -1, -1):
-            for j in range(len(grid[0]) - 1, -1, -1):
-                if i == len(grid) - 1 and j != len(grid[0]) - 1:
-                    dp[j] = grid[i][j] + dp[j + 1]
-                elif j == len(grid[0]) - 1 and i != len(grid) - 1:
-                    dp[j] = grid[i][j] + dp[j]
-                elif i != len(grid) - 1 and j != len(grid[0]) - 1:
-                    dp[j] = grid[i][j] + min(dp[j], dp[j + 1])
-                else:
-                    dp[j] = grid[i][j]
-        return dp[0]
-        
+        rows, cols = len(grid), len(grid[0])
+
+        res = [[float(inf)] * (cols + 1) for r in range(rows + 1)]
+        res[rows - 1][cols] = 0
+
+        for r in range(rows - 1, -1, -1):
+            for c in range(cols - 1, -1, -1):
+                res[r][c] = grid[r][c] + min(res[r + 1][c], res[r][c + 1])
+
+        return res[0][0] 
