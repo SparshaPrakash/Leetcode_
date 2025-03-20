e35c58eeddd85865aaf98e70a2088cf88c64dd7d
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-03-20 17:32:58 -0500

    Time: 3 ms (14.72%), Space: 18.1 MB (5.25%) - LeetHub

---
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # check first different character
+        # if word A is a prefix of word B, then word A should come before word B
+
+        order_map = {}
+        for index, val in enumerate(order):
+            order_map[val] = index
+
+        for i in range(len(words) - 1):
+            w1 = words[i]
+            w2 = words[i + 1]
+
+            for j in range(len(w1)):
+                if j == len(w2):
+                    return False
+                if w1[j] != w2[j]:
+                    if order_map[w2[j]] < order_map[w1[j]]:
+                        return False
+                    break
+
+        return True
+        
