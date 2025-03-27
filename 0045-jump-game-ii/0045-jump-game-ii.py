56f302ed05452c147d330e8fc98ec681fadfe405
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-03-27 11:47:59 -0500

    Time: 11 ms (44.7%), Space: 18.5 MB (74.8%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,33 +6,9 @@ class Solution:
         while r < len(nums) - 1:
             farthest = 0
             for i in range(l, r + 1):
-
                 farthest = max(farthest, i + nums[i])
             l = r + 1
             r = farthest
-            res +=1 
+            res += 1
 
         return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
