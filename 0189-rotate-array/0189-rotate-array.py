00a74c850019ee992cbfb57bf0638bcabf3a098c
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-03-26 19:23:45 -0500

    Time: 3 ms (71.09%), Space: 25.8 MB (21.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,22 +1,25 @@
+from typing import List
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        def reverse(l: int, r: int) -> None:
+            """Helper function to reverse nums between l and r."""
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l + 1, r - 1
+
+        # Step 1: Handle k greater than length of nums
         k = k % len(nums)
-        l, r = 0, len(nums) - 1
 
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        # Step 2: Reverse the entire array
+        reverse(0, len(nums) - 1)
 
-        l, r = 0, k -1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        # Step 3: Reverse the first k elements
+        reverse(0, k - 1)
 
-        l, r = k, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
-        
+        # Step 4: Reverse the rest of the array
+        reverse(k, len(nums) - 1)
