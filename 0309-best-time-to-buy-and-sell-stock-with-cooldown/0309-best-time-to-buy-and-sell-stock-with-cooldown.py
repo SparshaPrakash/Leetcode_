645e1b8648c9cec079e498caaddd1a1d1724830d
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-03-26 22:41:27 -0500

    Time: 9 ms (20.26%), Space: 19.1 MB (24.9%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,11 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # there are 2 states- buying or selling
-        # If we are buying- increament by 1 (i + 1)
-        # if we are selling- increament by 2 (i + 2)- since we aways need a cool down day after selling
-        
-        # hashmap to cache the results
-        dp = {} # Here, the key is (i, buying), val = max_profit
+        dp = {}
 
         def dfs(i, buying):
             if i >= len(prices):
@@ -14,17 +9,16 @@ class Solution:
                 return dp[(i, buying)]
 
             if buying:
-                buy = dfs(i + 1, not buying) - prices[i] # checking what the profit owuld be if we were buying
+                buy = dfs(i + 1, not buying) - prices[i]
                 cooldown = dfs(i + 1, buying)
                 dp[(i, buying)] = max(buy, cooldown)
+
             else:
                 sell = dfs(i + 2, not buying) + prices[i]
                 cooldown = dfs(i + 1, buying)
                 dp[(i, buying)] = max(sell, cooldown)
-
+       
             return dp[(i, buying)]
         return dfs(0, True)
 
-
-
-       
+        
