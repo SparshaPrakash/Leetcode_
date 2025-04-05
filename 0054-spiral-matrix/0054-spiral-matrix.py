4632680f7fe04d362512c5e6e11207ba3b1dad3f
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-04-05 15:19:18 -0500

    Time: 0 ms (100%), Space: 17.9 MB (18.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -5,26 +5,29 @@ class Solution:
         top, bottom = 0, len(matrix)
 
         while left < right and top < bottom:
-            # getting every i in the top row
+            # we get every i value in the top row
             for i in range(left, right):
                 res.append(matrix[top][i])
             top += 1
-            # getting every i in the right column
+
+            # we now get every i in the right col
             for i in range(top, bottom):
                 res.append(matrix[i][right - 1])
             right -= 1
 
-            if not(left < right and top < bottom):
+            # Check if there are any rows or columns left to process.
+            # If not, break the loop to avoid re-processing or accessing out-of-bounds elements.
+            if not (left < right and top < bottom):
                 break
-            # getting every i in the bottom row
+
+            # here we get every i value in the bottom row
             for i in range(right -1, left - 1, -1):
                 res.append(matrix[bottom - 1][i])
             bottom -= 1
-            # getting every i in the left colum
-            for i in range(bottom -1, top - 1, -1):
+
+            # get veery i in the left col
+            for i in range(bottom - 1, top - 1, -1):
                 res.append(matrix[i][left])
             left += 1
 
         return res
-        
-        
