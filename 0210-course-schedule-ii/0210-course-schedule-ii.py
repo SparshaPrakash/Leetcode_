c793c24e9f765e06b59ace25f58caa3b0ea28d57
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-06-06 22:52:52 -0500

    Time: 3 ms (77.32%), Space: 19.7 MB (24.66%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,39 +1,32 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # adjacency list
-        preMap = { c : [] for c in range(numCourses)}
+        prereq = { c:[] for c in range(numCourses)}
         for crs, pre in prerequisites:
-            preMap[crs].append(pre)
+            prereq[crs].append(pre)
 
-        # Visited- course has been added to the output
-        # Visiting- course not added to the output, but added to the solution
-        # Unvisited- cpurse not added to the output or to the cycle
 
         output = []
-        visited, cycle = set(), set()
-
+        visit, cycle = set(), set()
         def dfs(crs):
             if crs in cycle:
                 return False
-            if crs in visited:
+            if crs in visit:
                 return True
 
             cycle.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre):
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
                     return False
-            cycle.remove(crs)
-            visited.add(crs)
-            output.append(crs)
 
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
             return True
 
         for c in range(numCourses):
-            if not dfs(c):
+            if dfs(c) == False:
                 return []
+
         return output
-
-
-
-
+        
         
