c00a8230e0d14cfa597f3d84f73800f88fd9eb2a
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-06-10 17:56:43 -0500

    Time: 5996 ms (42.49%), Space: 19.2 MB (42.21%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,36 +1,34 @@
 class TrieNode:
     def __init__(self):
-        self.children = {} # can use a list as well
+        self.children = {}
         self.endOfWord = False
 
-    def addNode(self, word):
-        cur = self # root node
-
+    def addWord(self, word):
+        cur = self
         for c in word:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
-            cur= cur.children[c]
+            cur = cur.children[c]
         cur.endOfWord = True
-
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         root = TrieNode()
-
         for w in words:
-            root.addNode(w)
+            root.addWord(w)
 
-        rows, cols= len(board), len(board[0])
-        res, visited = set(), set()
+        rows, cols = len(board), len(board[0])
+        res, visit = set(), set()
 
-        def dfs(r, c, node, word):    # r, c- coordinates on the board, node- current character in Trie, word- word we have formed so far
-            if r < 0 or c < 0 or r == rows or c ==cols or board[r][c] not in node.children or (r, c) in visited:
-               return
-            visited.add((r,c))
 
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or r == rows or c == cols or
+                board[r][c] not in node.children or (r,c) in visit):
+                return
+
+            visit.add((r,c))
             node = node.children[board[r][c]]
             word += board[r][c]
-
             if node.endOfWord:
                 res.add(word)
 
@@ -39,13 +37,13 @@ class Solution:
             dfs(r, c + 1, node, word)
             dfs(r, c - 1, node, word)
 
-
-            visited.remove((r,c))
+            visit.remove((r,c)) # for backtracking
 
         for r in range(rows):
             for c in range(cols):
                 dfs(r, c, root, "")
 
         return list(res)
+
 
         
