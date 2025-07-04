ae19187aadeb699e41f60a9f6d40b6caec253614
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-04 17:26:46 -0500

    Time: 4 ms (39.31%), Space: 17.9 MB (62.81%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)  # -> adding the top level where we have to get to
+        cost.append(0) # the goal
 
         for i in range(len(cost) - 3, -1, -1):
-            cost[i] = min(cost[i] + cost[i + 1], cost[i]+ cost[i + 2])   # for each elemeny, we check how much it costs, to take one step from it and taking two steps from it to reach the top
+            cost[i] += min(cost[i + 1], cost[i + 2])
 
         return min(cost[0], cost[1])
