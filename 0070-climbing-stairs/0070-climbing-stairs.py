5403d33299de4f3a19700a97ebf3fff66c5c152d
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-04 16:56:06 -0500

    Time: 0 ms (100%), Space: 17.7 MB (48.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        dp = [0 for _ in range(n + 1)]
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
+        # any step we take should help us reach the top
+        # let'sstart from the end
+        # we can take both one step and two steps as long as the steps are in bounds
+        steps = [1, 2]
+        # initialize base cases
+        # 1 step only one way, 2 step 2 ways
+        # we need to write an equation which is a functio of the past value
+        # 3rd step -> 3 > sum of prev 2
+        # 4th step -> 3+2 ? = 5 
+        # 1111 112 211 22 121
+        for i in range(2, n):
+            steps.append(steps[i-1] + steps[i-2])
+        return steps[n-1]
