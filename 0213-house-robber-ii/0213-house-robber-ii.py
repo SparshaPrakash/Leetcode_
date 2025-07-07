5f74e996ae2a2714247327f41c7c2b1cb3e5bd14
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-07 15:08:23 -0500

    Time: 0 ms (100%), Space: 17.7 MB (86.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,16 +1,24 @@
 class Solution:
-    def helper(self, nums):
-        for i in range(1, len(nums)):
-            if i == 1:
-                nums[i] = max(nums[i], nums[0])
-            else:
-                nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
-        return nums[-1]
-
-
     def rob(self, nums: List[int]) -> int:
+        # use helper funtion
+        # pass parameters with first element included but not the last one
+        # do the same for forts element not inlcuded but last one included
+        # in ecah, check whats the max one an rob
+        # rob 1, rob2, n, n + 1, n + 2
+        # max of rob 1 + n or rob 2
         if len(nums) == 1:
             return nums[0]
+
         return max(self.helper(nums[1:]), self.helper(nums[:-1]))
 
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
+
+        return rob2
         
