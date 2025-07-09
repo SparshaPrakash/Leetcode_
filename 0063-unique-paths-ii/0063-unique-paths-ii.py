9de92854687d9674561b10f0c464fac8a5524e48
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-09 16:48:02 -0500

    Time: 0 ms (100%), Space: 18.1 MB (11.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -5,36 +5,28 @@ class Solution:
         m, n = len(grid), len(grid[0])
 
         # If the starting or ending cell is an obstacle, return 0 immediately
-        if grid[m - 1][n - 1] == 1 or grid[0][0] == 1:
+        if grid[0][0] == 1 or grid[m - 1][n - 1] == 1:
             return 0
 
-        # Create a 2D dp array initialized to 0
+        # Create DP table
         dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1  # 1 way to be at the start
 
-        # The bottom-right corner has 1 way if it's not an obstacle
-        dp[m - 1][n - 1] = 1
+        # Fill first row
+        for j in range(1, n):
+            if grid[0][j] == 0:
+                dp[0][j] = dp[0][j - 1]
+            # else it stays 0 (obstacle)
 
-        # Fill the last row (only moving right is possible)
-        for i in range(m - 2, -1, -1):
-            if grid[i][n - 1] == 1:
-                dp[i][n - 1] = 0  # Obstacle blocks path
-            else:
-                dp[i][n - 1] = dp[i + 1][n - 1]
+        # Fill first column
+        for i in range(1, m):
+            if grid[i][0] == 0:
+                dp[i][0] = dp[i - 1][0]
 
-        # Fill the last column (only moving down is possible)
-        for j in range(n - 2, -1, -1):
-            if grid[m - 1][j] == 1:
-                dp[m - 1][j] = 0  # Obstacle blocks path
-            else:
-                dp[m - 1][j] = dp[m - 1][j + 1]
+        # Fill the rest of the grid
+        for i in range(1, m):
+            for j in range(1, n):
+                if grid[i][j] == 0:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
-        # Fill the rest of the DP table
-        for i in range(m - 2, -1, -1):
-            for j in range(n - 2, -1, -1):
-                if grid[i][j] == 1:
-                    dp[i][j] = 0  # Obstacle blocks path
-                else:
-                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1]  # Sum of paths from below and right
-
-        # The result is stored in dp[0][0]
-        return dp[0][0]
+        return dp[m - 1][n - 1]
