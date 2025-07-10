b3e72640dc227b8593db26478802cf5478b776da
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-10 15:04:39 -0500

    Time: 12 ms (60.45%), Space: 20.9 MB (53.39%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,11 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # mapping charCpunt to list of Anagrams
+        dic = defaultdict(list)
 
-        for s in strs:
-            count = [0] * 26 # space for a to z
+        for word in strs:
+            dic["".join(sorted(word))].append(word)
 
-            for c in s:
-                count[ord(c) - ord("a")] += 1
+        return list(dic.values())
 
-            res[tuple(count)].append(s)
-
-        return list(res.values())
-
-
-    # this has a time complexity of O(m * n)
         
