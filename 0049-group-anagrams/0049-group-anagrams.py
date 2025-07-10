c97688d257a402ca6c07d76549aff4db312ed466
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-10 15:14:10 -0500

    Time: 17 ms (41.09%), Space: 22.8 MB (13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,16 @@
+from typing import List
 from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dic = defaultdict(list)
 
         for word in strs:
-            dic["".join(sorted(word))].append(word)
+            lst = [0] * 26  # List to count frequency of each letter a-z
+            for char in word:
+                lst[ord(char) - ord('a')] += 1  # Map 'a' to 0, 'b' to 1, etc.
 
-        return list(dic.values())
+            lst = tuple(lst)  # Tuples can be used as dictionary keys
+            dic[lst].append(word)
 
-        
+        return list(dic.values())  # Convert dict_values to list of lists
