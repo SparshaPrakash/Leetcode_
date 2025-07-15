cd3345b296f542fb08116a31057f2f9b8ad49316
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-07-15 16:33:41 -0500

    Time: 12 ms (91.18%), Space: 17.8 MB (87.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,19 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        n1 = len(s1)
+        n2 = len(s2)
+
+        if n1 > n2:
             return False
 
-        s1Count = [0] * 26
-        s2Count = [0] * 26
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
 
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
+        for i in range(n1):
+            s1_counts[ord(s1[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i]) - ord('a')] += 1
 
-        for i in range(len(s2) - len(s1)):
-            if s1Count == s2Count:
+        if s1_counts == s2_counts:
+            return True
+
+        for i in range(n1, n2):
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i - n1]) - ord('a')] -= 1
+            if s1_counts == s2_counts:
                 return True
-            s2Count[ord(s2[i]) - ord('a')] -= 1
-            s2Count[ord(s2[i + len(s1)]) - ord('a')] += 1
 
-        return s1Count == s2Count
+        return False 
+            
+        
