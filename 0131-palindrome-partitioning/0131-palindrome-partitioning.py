601a2cf8c825a9096fe8ebee72ebfb25996abd20
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-08-12 18:10:22 -0400

    Time: 43 ms (84.39%), Space: 34.7 MB (17.98%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -7,19 +7,25 @@ class Solution:
             if i >= len(s):
                 res.append(part.copy())
                 return
+
             for j in range(i, len(s)):
-                if self.isPalin(s, i, j):
-                    part.append(s[i:j + 1])
-                    dfs(j + 1)
+                if self.palin(s,i,j):
+                    part.append(s[i:j+1])
+                    dfs(j+1)
                     part.pop()
 
         dfs(0)
         return res
-    
-    def isPalin(self, s, l, r):
+
+
+    def palin(self, s, l, r):
         while l < r:
             if s[l] != s[r]:
                 return False
-            l, r = l + 1, r - 1
+
+            l, r= l +1, r - 1
+
         return True
+
             
+        
