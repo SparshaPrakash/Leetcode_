53f9c1053d0f98f290726161d422ccb7a2d0d8ce
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-08-12 18:56:24 -0400

    Time: 0 ms (100%), Space: 17.8 MB (69.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -7,22 +7,23 @@ class Solution:
             "4": "ghi",
             "5": "jkl",
             "6": "mno",
-            "7": "qprs",
+            "7": "pqrs",
             "8": "tuv",
             "9": "wxyz",
         }
 
-        def backtrack(i, cur):
-            if len(cur) == len(digits):
-                res.append(cur) 
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
                 return
-            
+
             for c in digitToChar[digits[i]]:
-                backtrack(i +1, cur + c)
+                backtrack(i +1, curStr + c)
 
         if digits:
             backtrack(0, "")
 
         return res
-
         
+
+    
