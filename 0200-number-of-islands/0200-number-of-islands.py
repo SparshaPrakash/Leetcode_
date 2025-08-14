124ca343732233088e594c2561c6e424d08f510f
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-08-14 18:30:34 -0400

    Time: 328 ms (6.68%), Space: 25.3 MB (30.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -8,27 +8,31 @@ class Solution:
         visited = set()
         islands = 0
 
-        def dfs(r,c):
-            if (
-                r < 0 or r >= rows or 
-                c < 0 or c >= cols or 
-                grid[r][c] != '1' or 
-                (r, c) in visited
-            ):
-                return
-
+        def bfs(r,c):
+            q = collections.deque()
             visited.add((r,c))
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            q.append((r,c))
 
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+                for dr, dc in directions:
+                    r, c = row +dr, col + dc
+                    if (r in range(rows) and c in range(cols) and (r,c) not in visited and
+                        grid[r][c] == "1"):
+                        q.append((r,c))
+                        visited.add((r,c))
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visited:
-                    dfs(r,c)
-                    islands += 1
+                    bfs(r,c)
+                    islands +=1
 
         return islands
 
-                
+
+
+            
