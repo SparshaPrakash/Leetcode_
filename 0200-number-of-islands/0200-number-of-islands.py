ab1952500ca862ca5a8cc1800da4001090f8b41f
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-08-14 18:18:58 -0400

    Time: 289 ms (17.76%), Space: 25.7 MB (15.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,11 +3,12 @@ class Solution:
         if not grid:
             return 0
 
+
         rows, cols = len(grid), len(grid[0])
         visited = set()
         islands = 0
 
-        def dfs(r, c):
+        def dfs(r,c):
             if (
                 r < 0 or r >= rows or 
                 c < 0 or c >= cols or 
@@ -15,15 +16,19 @@ class Solution:
                 (r, c) in visited
             ):
                 return
-            visited.add((r, c))
+
+            visited.add((r,c))
             directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
             for dr, dc in directions:
                 dfs(r + dr, c + dc)
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visited:
-                    dfs(r, c)
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    dfs(r,c)
                     islands += 1
 
         return islands
+
+                
