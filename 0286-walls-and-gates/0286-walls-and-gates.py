97403b588c70077f1711e886b3eb7539daa7bc27
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-08-15 17:31:20 -0400

    Time: 59 ms (36.54%), Space: 24.9 MB (29.6%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -5,30 +5,31 @@ class Solution:
         """
         rows, cols = len(rooms), len(rooms[0])
         visited = set()
-        q = deque()
+        q = collections.deque()
 
-        def addRoom(r, c):
-            if (r < 0 or r == rows or c < 0 or c == cols or (r, c) in visited or rooms[r][c] == -1):
+
+        def addRoom(r,c):
+            if (r < 0 or r == rows or c< 0 or c == cols or (r,c) in visited or rooms[r][c] == -1):
                 return
-            visited.add((r, c))
-            q.append((r, c))
-
+            visited.add((r,c))
+            q.append((r,c))
 
         for r in range(rows):
             for c in range(cols):
                 if rooms[r][c] == 0:
-                    q.append((r, c))
-                    visited.add((r, c))
+                    q.append((r,c))
+                    visited.add((r,c))
 
         dist = 0
         while q:
             for i in range(len(q)):
-                (r, c) = q.popleft()
+                r,c = q.popleft()
                 rooms[r][c] = dist
-                addRoom(r + 1, c)
-                addRoom(r - 1, c)
-                addRoom(r, c + 1)
-                addRoom(r, c - 1)
+                addRoom(r +1, c)
+                addRoom(r -1, c)
+                addRoom(r , c + 1)
+                addRoom(r, c- 1)
 
             dist += 1
-        
+
+
