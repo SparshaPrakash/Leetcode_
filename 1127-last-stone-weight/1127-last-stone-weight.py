643763af4301a3f849f98cdd138eb43b3fbd20b1
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-09-11 10:57:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (32.34%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -1,15 +1,16 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        stones = [-s for s in stones] 
+        stones = [-s for s in stones]
         heapq.heapify(stones)
 
         while len(stones) > 1:
-            first = heapq.heappop(stones)  # heaviest stone
+            first = heapq.heappop(stones)
             second = heapq.heappop(stones)
-            if second > first:   # it owuld actually have to be if forst > secons, but since we added a neg sign, we check second > first
-                heapq.heappush(stones, first - second)
-        
-        stones.append(0)  # if there is no stoone left
-        return abs(stones[0])
 
+            if second > first:
+                heapq.heappush(stones, first - second)
+
+
+        stones.append(0)
+        return abs(stones[0])
         
