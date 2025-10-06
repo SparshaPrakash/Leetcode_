e9c7573bec44b296abee51714219b9f56c531194
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-10-06 12:14:52 -0400

    Time: 2 ms (98.92%), Space: 45.4 MB (5.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (hashMap.containsKey(complement)) {
+                return new int[] { i, hashMap.get(complement) };
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        return new int[] {};  // return empty array if not found
+    }
+}
