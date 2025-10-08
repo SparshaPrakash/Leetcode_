8fcb142066374cf1072aadedf10df731f15a91e5
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-10-07 23:22:05 -0400

    Time: 16 ms (27.63%), Space: 48.4 MB (23.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,19 +1,31 @@
+import java.util.*;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        
-        for (String word : strs) {
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars);
-            
-            if (!map.containsKey(sortedWord)) {
-                map.put(sortedWord, new ArrayList<>());
+
+        for (String s : strs) {
+            int[] freq = new int[26]; // only lowercase 'a'..'z'
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                // if inputs may have uppercase, uncomment next line:
+                // c = Character.toLowerCase(c);
+                freq[c - 'a']++;
             }
-            
-            map.get(sortedWord).add(word);
+
+            String key = buildKey(freq); // value-based key from counts
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-        
+
         return new ArrayList<>(map.values());
+    }
+
+    // Build a compact, collision-free key like "#1#0#0#2...#0"
+    private String buildKey(int[] freq) {
+        StringBuilder sb = new StringBuilder(26 * 2);
+        for (int i = 0; i < 26; i++) {
+            sb.append('#').append(freq[i]);
+        }
+        return sb.toString();
     }
 }
