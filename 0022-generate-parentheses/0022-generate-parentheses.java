dc00c12a310c6f133fce09748ce3976274451f28
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-10-11 19:23:20 -0400

    Time: 2 ms (43.85%), Space: 43.8 MB (13.79%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -3,24 +3,20 @@ import java.util.*;
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backtrack(n, n, new StringBuilder(), res);
+        dfs(n, n, "", res);
         return res;
     }
 
-    private void backtrack(int openLeft, int closeLeft, StringBuilder sb, List<String> res) {
+    private void dfs(int openLeft, int closeLeft, String cur, List<String> res) {
         if (openLeft == 0 && closeLeft == 0) {
-            res.add(sb.toString());
+            res.add(cur);
             return;
         }
-        if (openLeft > 0) {                // you can always place '(' if any are left
-            sb.append('(');
-            backtrack(openLeft - 1, closeLeft, sb, res);
-            sb.setLength(sb.length() - 1); // pop
+        if (openLeft > 0) {
+            dfs(openLeft - 1, closeLeft, cur + "(", res);
         }
-        if (closeLeft > openLeft) {        // only place ')' if it won't break validity
-            sb.append(')');
-            backtrack(openLeft, closeLeft - 1, sb, res);
-            sb.setLength(sb.length() - 1); // pop
+        if (closeLeft > openLeft) {
+            dfs(openLeft, closeLeft - 1, cur + ")", res);
         }
     }
 }
