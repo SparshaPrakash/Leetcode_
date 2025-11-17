7064682202aa1e25559c45e9a1a65957d6522410
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-11-17 11:35:06 -0500

    Time: 14 ms (38.16%), Space: 19.8 MB (20.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,20 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        d = defaultdict(int)
-        for num in nums:
-            d[num] += 1  # d = {1:3, 2:2,3:1, 4:3}
-        heap = []  
-        for key, val in d.items():
-            if len(heap) < k or val > heap[0][0]:  # if the value is greater than the mimimum value in the heap
-                heapq.heappush(heap,[val,key]) # val first here, vause it is sorted based on the firts value
-            if len(heap) > k:
-                heapq.heappop(heap)
+from collections import defaultdict
 
-        return [i[1] for i in heap] # retrning the values in the heap
+class Solution:
+    def topKFrequent(self, nums, k):
+        freq = defaultdict(int)
+        for num in nums:
+            freq[num] += 1
+
+        # buckets[index] = list of numbers with frequency = index
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for num, f in freq.items():
+            buckets[f].append(num)
+
+        res = []
+        # iterate frequencies from high to low
+        for f in range(len(nums), 0, -1):
+            for num in buckets[f]:
+                res.append(num)
+                if len(res) == k:
+                    return res
