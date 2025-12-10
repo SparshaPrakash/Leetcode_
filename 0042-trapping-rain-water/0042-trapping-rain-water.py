83b3f5549c1f278b0c24e6aecbbc8c7cb914d034
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-12-10 17:47:51 -0500

    Time: 12 ms (58.86%), Space: 19.3 MB (38.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -9,9 +9,17 @@ class Solution:
                 left += 1
                 left_max = max(left_max, height[left])
                 water_trapped += left_max - height[left]
+
             else:
                 right -= 1
                 right_max = max(right_max, height[right])
                 water_trapped += right_max - height[right]
 
         return water_trapped
+
+
+
+
+
+
+       
