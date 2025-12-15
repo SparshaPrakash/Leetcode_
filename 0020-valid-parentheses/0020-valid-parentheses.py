939cd4b0052634b3b684ba974a41cd772433e9a1
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2025-12-15 17:37:41 -0500

    Time: 3 ms (35.99%), Space: 18 MB (36.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,34 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+
         d = {")":"(", "}":"{", "]":"["}
 
         for val in s:
-            if val in d:  # if it is a closing brcaket
+            if val in d:
                 if stack and stack[-1] == d[val]:
                     stack.pop()
+
                 else:
                     return False
-
 
             else:
                 stack.append(val)
 
         return True if not stack else False
+
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
