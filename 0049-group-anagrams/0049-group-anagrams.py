216f17cf8816784f274a8816e944fdd0508adbb4
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2026-04-01 21:20:27 -0400

    Time: 7 ms (98.52%), Space: 22 MB (73.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,10 +2,16 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = defaultdict(list)
+        # defaultdict prevents KeyError if the key doesn't exist yet
+        anagram_map = defaultdict(list)
 
-        for word in strs:
-            key = "".join(sorted(word))  # e.g. "eat" -> "aet"
-            dic[key].append(word)
+        for s in strs:
+            # Sort the string to create a 'signature'
+            # sorted("eat") returns ['a', 'e', 't'], so we join it into a string
+            sorted_s = "".join(sorted(s))
+            
+            # Add the original string to the list corresponding to that signature
+            anagram_map[sorted_s].append(s)
 
-        return list(dic.values())
+        # Return just the values (the groups of anagrams)
+        return list(anagram_map.values())
