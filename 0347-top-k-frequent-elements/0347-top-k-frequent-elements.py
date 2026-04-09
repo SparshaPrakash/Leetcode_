72a94d918fec5a60a9a9d3cfeaac70037c09964b
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2026-04-09 11:27:16 -0400

    Time: 1 ms (92.24%), Space: 22.9 MB (68.6%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,11 @@
-from collections import defaultdict
+import heapq
+from collections import Counter
+from typing import List
 
 class Solution:
-    def topKFrequent(self, nums, k):
-        freq = defaultdict(int)
-        for num in nums:
-            freq[num] += 1
-
-        # buckets[index] = list of numbers with frequency = index
-        buckets = [[] for _ in range(len(nums) + 1)]
-        for num, f in freq.items():
-            buckets[f].append(num)
-
-        res = []
-        # iterate frequencies from high to low
-        for f in range(len(nums), 0, -1):
-            for num in buckets[f]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 1. Count frequencies: {number: count}
+        count = Counter(nums)
+        
+        # 2. Return the top k elements
+        return heapq.nlargest(k, count.keys(), key=count.get)
