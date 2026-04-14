1a8d2ad86c94c8a015e9d42d06aa20276d41d8ef
Sparsha Prakash <159576290+SparshaPrakash@users.noreply.github.com>
2026-04-14 19:45:16 -0400

    Time: 15 ms (94.24%), Space: 25.5 MB (57.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,29 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # output array which is initially also the rigt prefix array
-        output= [1]
-        for i in range(len(nums) - 1, 0, -1):
-            output.append(output[-1] * nums[i])
-
-        output = output[::-1]
-
-        left = 1
-        for i in range(len(nums)):
-            output[i] = output[i] * left
-            left *= nums[i]
-
-
-        return output
-
-
-
-
-
-
-
-
-
-
-
-
-       
+        n = len(nums)
+        res = [1] * n
+        
+        # Pass 1: Calculate Prefix Products
+        # res[i] will store the product of all elements to the left of i
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+            
+        # Pass 2: Calculate Suffix Products and multiply with Prefix
+        # suffix will store the product of all elements to the right of i
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+            
+        return res
